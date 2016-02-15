239f187d08d0ad6889f957137c0dcec1178469a9
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-15 15:03:45 -0200

    :art: ONEPROJECT-579 ONEPROJECT-607 ONEPROJECT-608 Adiciona comentários para javadoc

--- src/DataContracts/Person/GetBuyerResponseData.java
+++ src/DataContracts/Person/GetBuyerResponseData.java
@@ -13,22 +13,44 @@ import java.util.UUID;
  * @author munir
  */
 public class GetBuyerResponseData extends BaseResponse {
+    /**
+     * Chave do Comprador
+     */
     private UUID BuyerKey;
     
+    /**
+     * Indicador de sucesso
+     */
     private Boolean Success;
 
+    /**
+     * Retorna a chave do comprador
+     * @return 
+     */
     public UUID getBuyerKey() {
         return BuyerKey;
     }
 
+    /**
+     * Define a chave do comprador
+     * @param BuyerKey 
+     */
     public void setBuyerKey(UUID BuyerKey) {
         this.BuyerKey = BuyerKey;
     }
 
+    /**
+     * Retorna o sucesso
+     * @return 
+     */
     public Boolean getSuccess() {
         return Success;
     }
 
+    /**
+     * Define o Sucesso
+     * @param Success 
+     */
     public void setSuccess(Boolean Success) {
         this.Success = Success;
     }
