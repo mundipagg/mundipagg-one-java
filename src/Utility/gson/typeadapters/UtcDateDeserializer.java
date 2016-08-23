9e19129174deac2ded0d9dade898047f8dc6b4db
Rafael Cordeiro <rcordeiro@mundipagg.com>
2016-08-23 19:19:58 -0300

    Ajuste Datas são parseadas com Timezone corretos.

---
+++ src/Utility/gson/typeadapters/UtcDateDeserializer.java
@@ -0,0 +1,31 @@
+package Utility.gson.typeadapters;
+
+
+import DataContracts.ServiceConstants;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class UtcDateDeserializer implements JsonDeserializer<Date> {
+    @Override
+    public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
+        String date = element.getAsString();
+
+        SimpleDateFormat formatter = new SimpleDateFormat(ServiceConstants.DATE_TIME_FORMAT);
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        try {
+            return formatter.parse(date);
+        } catch (ParseException e) {
+            throw new JsonParseException(e);
+        }
+    }
+}
+
